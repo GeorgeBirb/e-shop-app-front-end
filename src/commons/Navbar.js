2d84379de5e3c0a31f3fa0dda8f1de70e2542de2
George Birbakos <niraeus@gmail.com>
2023-02-08 18:15:03 +0200

    Initial Commit

---
+++ src/commons/Navbar.js
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom'
+import UserService from "../services/UserService";
+import '../App.css'
+import { useEffect, useState } from 'react';
+
+function Navbar() {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    setUserName(UserService.getUsername());
+  }, []);
+
+  useEffect(() => {
+    console.log('userName', userName);
+  }, [userName]);
+
+  return (
+
+    <div className="nav-bar-container-light">
+      <div className='alignLeft'>
+        <h2>Welcome,</h2>
+        <h2>{userName}!!</h2>
+      </div>
+      <div>
+        <ul className="middle-items">
+          <li className="list-item">
+            <Link to="/home" className="link-light">
+              Home
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link to="/shop" className="link-light">
+              Shops
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link to="/shopCategory" className="link-light">
+              ShopCategory
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link to="/about" className="link-light">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className='alignRight'>
+        <button className="buttonLogout" onClick={() => UserService.doLogout()}>
+          <h2>Logout</h2>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
